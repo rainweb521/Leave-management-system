6f05cde2f96ed6be6d76475d9c925f5586711548
魏春雨 <nylrain@163.com>
2017-08-09 16:15:32 +0800

    写好了请假条的模板，已经修改了首页的信息，菜单栏等设置

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    //'配置项'=>'配置值'
+
+    //URL地址不区分大小写
+    'URL_CASE_INSENSITIVE' =>true,
+    'URL_MODEL'=>0,
+    'LOAD_EXT_CONFIG' => 'db',
+    'MD5_PRE' => 'sing_cms',
+    'HTML_FILE_SUFFIX' => '.html',
+);
---
+++ Application/Home/Controller/CommonController.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rain
+ * Date: 2017/8/9
+ * Time: 11:34
+ */
+
+namespace Home\Controller;
+use Think\Controller;
+
+class CommonController extends Controller {
+
+}
